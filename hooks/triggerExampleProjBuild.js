657bd25709de956c66108884652ca85d874d0b43
Dave Alden <dpa99c@gmail.com>
2016-11-26 17:26:54 +0000

    Convert example project build trigger from typescript to javascript to avoid ts validation problems

--- hooks/triggerExampleProjBuild.js
+++ hooks/triggerExampleProjBuild.js
@@ -1,17 +1,13 @@
 #!/usr/bin/env node
 "use strict";
 
-declare var require: any;
-declare var __dirname: any;
-declare var process: any;
-
-const shell = require('shelljs'),
-  path = require('path'),
-  got = require('got');
+var shell = require('shelljs');
+var path = require('path');
+var got = require('got');
 
 const targetRepo = 'dpa99c/cordova-custom-config-example';
 
-console.log(`Fetching Git commit hash...`);
+console.log("Fetching Git commit hash...");
 
 const gitCommitRet = shell.exec('git rev-parse HEAD', {
   cwd: path.join(__dirname, '..')
@@ -25,28 +21,28 @@ if (0 !== gitCommitRet.code) {
 
 const gitCommitHash = gitCommitRet.stdout.trim();
 
-console.log(`Git commit: ${gitCommitHash}`);
+console.log("Git commit: ${gitCommitHash}");
 
 console.log('Calling Travis...');
 
-got.post(`https://api.travis-ci.org/repo/`+encodeURIComponent(targetRepo)+`/requests`, {
+got.post("https://api.travis-ci.org/repo/"+encodeURIComponent(targetRepo)+"/requests", {
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
     "Travis-API-Version": "3",
-    "Authorization": `token ${process.env.TRAVIS_API_TOKEN}`,
+    "Authorization": "token ${process.env.TRAVIS_API_TOKEN}",
   },
   body: JSON.stringify({
     request: {
-      message: `Trigger build at `+targetRepo+` commit: ${gitCommitHash}`,
+      message: "Trigger build at "+targetRepo+" commit: ${gitCommitHash}",
       branch: 'master',
-    },    
-  }),
+    }
+  })
 })
-.then(() => {
+.then(function(){
   console.log("Triggered build of "+targetRepo);
 })
-.catch((err) => {
+.catch(function(){
   console.error(err);
   
   process.exit(-1);
